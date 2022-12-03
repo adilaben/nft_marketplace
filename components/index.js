250dee174914440a87d83d6ec1be9bc28356a95e
Adila Bendahou <adilabendahou98@gmail.com>
2022-12-03 18:39:57 +0100

    NFT details screen

--- components/index.js
+++ components/index.js
@@ -1,12 +1,16 @@
 import HomeHeader from './HomeHeader';
 import FocusedStatusBar from './FocusedStatusBar';
 import NFTCard from './NFTCard';
+import DetailsBid from './DetailsBid';
+import DetailsDesc from './DetailsDesc';
 import { CircleButton, RectButton } from './Button';
 import { SubInfo, People, EndDate, EthPrice, NFTTitle } from "./SubInfo";
 
 export {
     HomeHeader, 
     NFTCard, 
+    DetailsBid,
+    DetailsDesc,
     FocusedStatusBar, 
     SubInfo,
     People,
